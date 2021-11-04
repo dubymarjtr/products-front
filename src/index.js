fc011bbfb55e784bd954cfc0c963255de49ef73c
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-11-03 21:51:15 -0500

    delete starter api.index()

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import Form from "./components/Form";
 import List from "./components/List";
 import api from "./services/api.service";
 
-api.index().then((data) => {
-  console.log(data);
-});
 const root = document.querySelector("#root");
 
 api.index().then((products) => {
